90f158a93cadbabe15ee8aea971183d7ab515d7a
sydrob <55166840+sydrobhez@users.noreply.github.com>
2021-11-19 17:44:34 -0600

    updated event listeners

--- script.js
+++ script.js
@@ -5,19 +5,22 @@ let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 let photosArray = [];
+let initialLoad = true;
 
 // Unsplash API
-const count = 30;
+let count = 5;
 const apiKey = 'ZV5WDXqxsYPlTZSGI-S8MV4Ct9pil_3jXAHYhB1hzEU';
-const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 //Check if all images were loaded
 function imageLoaded() {
-    console.log('image loaded');
-    imageLoaded++;
-    if(imageLoaded === totalImages) {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages) {
         ready = true;
-        console.log('ready =', ready);
+        loader.hidden = true;
+        initialLoad = false;
+        count = 30;
+        apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
     }
 }
 
@@ -30,6 +33,9 @@ function setAttributes(element, attributes) {
 
 //Create elements for links and photos, add to the DOM
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images', totalImages);
     //Run function for each object in photosArray
     photosArray.forEach((photo) => {
         //Create <a> to link to Unsplash
@@ -66,7 +72,8 @@ async function getPhotos () {
 
 //Check to see if scrolling near bottomo of page,Load more photos
 window.addEventListener('scroll',() => {
-    if(window.innerHeight + window.scrollY >= document.body.offsetHeight -1000) {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight -1000 && ready) {
+        ready = false;
         getPhotos();
     }
 })
